40f8e0a183f4713028fab36a92f4c892dc8662c7
Bryan Lowe <mr.bryan.lowe@gmail.com>
2017-08-03 20:45:13 -0700

    Adjusting the template to suit Market View

---
+++ src/pages/page-elements/topbar-menu.ts
@@ -0,0 +1,24 @@
+import * as $ from 'jquery';
+
+
+export class TopbarMenu {
+ 
+    toggleClicked(event: MouseEvent)
+    {
+        var target = event.srcElement.id;
+        var body = $('body');
+        var menu = $('#sidebar-menu');
+        
+        // toggle small or large menu
+        if (body.hasClass('nav-md')) {
+            menu.find('li.active ul').hide();
+            menu.find('li.active').addClass('active-sm').removeClass('active');
+        } else {
+            menu.find('li.active-sm ul').show();
+            menu.find('li.active-sm').addClass('active').removeClass('active-sm');
+        }
+        body.toggleClass('nav-md nav-sm');
+
+    }
+
+}
--- src/resources/index.ts
+++ src/resources/index.ts
@@ -2,5 +2,7 @@ import {FrameworkConfiguration} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
+  	// page elements
+  	//'./page-elements/sidebar-menu',
   ]);
 }
